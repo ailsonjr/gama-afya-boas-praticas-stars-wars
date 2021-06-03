c96bce7fcecb27cb3011b667ec7f4e9ba1d89dda
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-06-02 23:50:00 -0300

    Adicionado ícones com links para o github e o linkedin no footer

--- src/views/components/footer/index.js
+++ src/views/components/footer/index.js
@@ -1,11 +1,22 @@
 import Planet from '../../../assets/images/elements/planet.png'
+import Github from '../../../img/github.png'
+import Linkedin from '../../../img/linkedin.png'
 
 let footer = `
 <div class="footer-contain">
     <p>
-        Star wars  -  este é um exemplo criado para os Alunos Gama Academy</br>
+        Star wars  -  este é um exemplo criado por Ailson P Lima Jr</br>
         Módulo de boas práticas.
     </p>
+    <div class="social">
+        <a href="https://github.com/ailsonjr/gama-afya-boas-praticas-stars-wars" target="blank">
+            <img src="${Github}" alt="github">
+        </a>
+        </p>
+        <a href="https://www.linkedin.com/in/ailsonjr/" target="blank">
+            <img src="${Linkedin}" alt="linkedin">
+        </a>
+    </div>
     <a href="#section-one">
         <img src="${Planet}" alt="planet">
     </a>
